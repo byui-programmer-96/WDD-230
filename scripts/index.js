2da8d4950a27e120a1d0e632bb514c90f575bec7
byui-programmer-96 <bam21002@byui.edu>
2023-01-13 10:32:46 -0600

    ...

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,16 @@
+
+
+//dark mode
+
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
+
 
 modeButton.addEventListener("click", () => {
 	if (modeButton.textContent.includes("☀️")) {
 		main.style.background = "#000";
 		main.style.color = "#fff";
-		modeButton.textContent = "🌙 ";
+		modeButton.textContent = "🌙";
 	} else {
 		main.style.background = "#eee";
 		main.style.color = "#000";
