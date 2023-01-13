cfa17f7692a7244960ab3b044996888f11fb3dce
byui-programmer-96 <bam21002@byui.edu>
2023-01-12 23:51:44 -0600

    .....

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,13 @@ const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("☑️")) {
+	if (modeButton.textContent.includes("☀️")) {
 		main.style.background = "#000";
 		main.style.color = "#fff";
-		modeButton.textContent = "❎";
+		modeButton.textContent = "🌙 ";
 	} else {
 		main.style.background = "#eee";
 		main.style.color = "#000";
-		modeButton.textContent = "☑️";
+		modeButton.textContent = "☀️";
 	}
 });
