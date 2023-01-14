8ad94e745a913efc89fa48b158ef5f42e8595128
byui-programmer-96 <bam21002@byui.edu>
2023-01-13 22:59:26 -0600

    ...

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,15 +1,16 @@
+// Dark Mode
+
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("☑️")) {
+	if (modeButton.textContent.includes("dark_mode")) {
 		main.style.background = "#000";
 		main.style.color = "#fff";
-		modeButton.textContent = "❎";
+		modeButton.innerHTML = '<span class="material-symbols-outlined">brightness_5</span>'
 	} else {
 		main.style.background = "#eee";
 		main.style.color = "#000";
-		modeButton.textContent = "☑️";
+		modeButton.innerHTML = '<span class="material-symbols-outlined">dark_mode</span>'
 	}
 });
-
