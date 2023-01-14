7406bb94641004a715ad46484d7b340a9ffdb5c0
byui-programmer-96 <bam21002@byui.edu>
2023-01-13 21:28:21 -0600

    ....

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,15 +1,15 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
-
 modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("☀️")) {
+	if (modeButton.textContent.includes("☑️")) {
 		main.style.background = "#000";
 		main.style.color = "#fff";
-		modeButton.textContent = "🌙";
+		modeButton.textContent = "❎";
 	} else {
 		main.style.background = "#eee";
 		main.style.color = "#000";
-		modeButton.textContent = "☀️";
+		modeButton.textContent = "☑️";
 	}
 });
+
