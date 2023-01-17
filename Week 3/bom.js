0571a5e224fb96e84a89118fd6f43f7e9a588643
byui-programmer-96 <bam21002@byui.edu>
2023-01-17 15:13:24 -0600

    bom w3

---
+++ Week 3/bom.js
@@ -0,0 +1,24 @@
+const list = document.querySelector(".list");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  let chapter = input.value;
+  input.value = "";
+
+  const listItem = document.createElement("li");
+  const listText = document.createElement("span");
+  const listBtn = document.createElement("button");
+
+  listItem.appendChild(listText);
+  listText.textContent = chapter;
+  listItem.appendChild(listBtn);
+  listBtn.innerHTML = "&#10060;";
+  list.appendChild(listItem);
+
+  listBtn.addEventListener("click", function () {
+    list.removeChild(listItem);
+  });
+
+  input.focus();
+});
