3764d1358cb01a9d041dacf5bb6bf620a73337a8
byui-programmer-96 <bam21002@byui.edu>
2023-01-19 14:57:00 -0600

    visits

---
+++ scripts/visit.js
@@ -0,0 +1,13 @@
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
