01ff575a6cb206257782f607893d1c2c60760f65
byui-programmer-96 <bam21002@byui.edu>
2023-01-19 15:19:21 -0600

    chamber

---
+++ chamber/scripts/dates.js
@@ -0,0 +1,6 @@
+var date = new Date().getFullYear();
+document.getElementById("year").innerHTML = date;
+document.write(new Date().getFullYear());
+
+var date = document.lastModified;
+    document.getElementById("lastModified").innerHTML = "Updated : " + date;
