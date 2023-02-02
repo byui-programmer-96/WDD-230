c2c2b754c71f11424f5e76a87eb3bc699d3c1209
byui-programmer-96 <bam21002@byui.edu>
2023-02-02 13:39:43 -0600

    week5

---
+++ Week 5/scripts/weather.js
@@ -0,0 +1,20 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/___________';
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // testing only
+        // displayData(data); // uncomment when ready
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
